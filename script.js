e994e6e18f2a55bf950bfe6286b5ee8bdf30ad36
loetitia <audiger.loetitia@gmail.com>
2022-09-15 14:33:09 +0200

    changement ok

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
+function IMC() {
+  var poids = parseInt(document.getElementById("poids").value);
+  var taille = parseInt(document.getElementById("taille").value);
+  var imc = (poids / ((taille * taille) / 10000)).toFixed(2);
 
-function IMC(){
-    var poids = document.getElementById("poids").value;
-    var taille = document.getElementById("taille").value;
-    var imc = poids / (taille * taille);
-    imc = imc.toFixed(2);
-
-if(imc < 16){
-    document.getElementById("result2").innerHTML = "risque  famine";
-}
-else if(imc < 18.5){
-    document.getElementById("result2").innerHTML = "vous êtes en maigreur";
-}
-else if(imc < 25){
-    document.getElementById("result2").innerHTML = "vous êtes en corpulence normale";
-}
-else if(imc < 30){
+  if (imc < 16) {
+    document.getElementById("result2").innerHTML =
+      "vous etes en dessous du seuil de maigreur";
+  } else if (imc < 18.5) {
+    document.getElementById("result2").innerHTML = "vous êtes  maigre";
+  } else if (imc < 25) {
+    document.getElementById("result2").innerHTML =
+      "vous êtes en corpulence normale";
+  } else if (imc < 30) {
     document.getElementById("result2").innerHTML = "vous êtes en surpoids";
+  } else if (imc < 35) {
+    document.getElementById("result2").innerHTML =
+      "vous êtes en obésité modérée";
+  } else if (imc < 40) {
+    document.getElementById("result2").innerHTML =
+      "vous êtes en obésité sévère";
+  } else {
+    document.getElementById("result2").innerHTML =
+      "vous êtes en obésité morbide";
+  }
+  document.getElementById("result").innerHTML = "votre imc est de " + imc;
 }
-else if(imc < 35){
-    document.getElementById("result2").innerHTML = "vous êtes en obésité modérée";
-}
-else if(imc < 40){
-    document.getElementById("result2").innerHTML = "vous êtes en obésité sévère";
-}
-else{
-    document.getElementById("result2").innerHTML = "vous êtes en obésité morbide";
-}
-document.getElementById("result").innerHTML = "votre imc est de " + imc;
-    
-
-}
-
